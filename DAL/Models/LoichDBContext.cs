4437e13e349d4ae069ea54df00326afeaf464f0b
Khôi Lê Đạt Anh <khoi2k18@gmail.com>
2021-11-29 10:27:37 +0700

    [29/11/2021 - KhoiLDA] Commit to from master

--- DAL/Models/LoichDBContext.cs
+++ DAL/Models/LoichDBContext.cs
@@ -43,7 +43,7 @@ namespace DAL.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=localhost;Database=LoichDB;User ID=sa;pwd=Hanquang@123;");
+                optionsBuilder.UseSqlServer("Server=SE140074;Database=LoichDB;User ID=sa;pwd=belo1405;");
             }
         }
 
